d0dfbf29da35fddfa3a9361d73356c6cc790bf48
Jacek Kosinski <kashiash@gmail.com>
2024-05-09 09:49:36 +0200

    Initial Commit

---
+++ CoordinatorExample/CoordinatorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorExampleApp.swift
+//  CoordinatorExample
+//
+//  Created by Jacek Kosinski U on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
